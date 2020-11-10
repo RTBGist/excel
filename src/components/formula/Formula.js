7b6d54cf866e20b74caf243c1fc129617a2bdea3
RTBGist <YourDeveloper@mail.ru>
2020-11-11 03:46:54 +0700

    finish base table logic (#5)

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,12 +1,14 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click'],
+      listeners: ['input', 'keydown'],
+      ...options, // emitter и прочее
     })
   }
 
@@ -14,15 +16,36 @@ export class Formula extends ExcelComponent {
   toHTML() {
     return `
     <div class="info">fx</div>
-    <div class="input" contenteditable spellcheck="false"></div>`
+    <div id="formula" class="input" contenteditable spellcheck="false"></div>`
+  }
+
+  init() {
+    super.init()
+
+    this.$formula = this.$root.find('#formula')
+
+    this.$on('table:select', ($cell) => {
+      this.$formula.text($cell.text())
+    })
+
+    this.$on('table:click', ($cell) => {
+      this.$formula.text($cell.text())
+    })
+
+    this.$on('table:input', ($cell) => {
+      this.$formula.text($cell.text())
+    })
   }
 
   onInput(event) {
-    console.log(this.$root)
-    console.log('Formula: onInput', event)
+    this.$emit('formula:input', $(event.target).text())
   }
-  onClick() {
-    console.log('click')
+  onKeydown(event) {
+    const codes = ['Enter', 'Tab']
+    if (codes.includes(event.code)) {
+      event.preventDefault()
+      this.$emit('formula:done')
+    }
   }
 }
 
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,19 +3,37 @@ import {DomListener} from '@core/DomListener';
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
-    this.name = options.name || 'Name is not implemented'
-  }
+    this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
 
+    this.prepare()
+  }
+  // Настраиваем наш компонент до init
+  prepare() {
+
+  }
+  // Уведомления слушателей о случившимся событии
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
-
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
   init() {
-    this.initDOMListeners() // Добавляем listeners компонентам
+    this.initDOMListeners()
   }
-
+  // Удаляем компонент и DOM слушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach((unsub) => unsub())
   }
 }
