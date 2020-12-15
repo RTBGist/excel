25d06ca950a3ad4a1a2a412f409bed373b646bee
Yourdeveloper <Yourdeveloper@mail.ru>
2020-12-15 23:43:58 +0700

    Routing implemented

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent';
 import {changeTitle} from '@/redux/actions';
 import {$} from '@core/dom';
 import {defaultTitle} from '@/constants';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -9,9 +10,23 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     })
+  }
+
+  onClick(event) {
+    const $target = $(event.target)
+
+    if ($target.data.button === 'remove') { // удаляем таблицу
+      const decision = confirm('Вы действительно хотите удалить таблицу?')
+      if (decision) {
+        localStorage.removeItem('excel:' + ActiveRoute.param) // удаляем из кэша по адресу
+        ActiveRoute.navigate('') // перекидываем на главную
+      }
+    } else if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('')
+    }
   }
 
   onInput(event) {
@@ -24,11 +39,11 @@ export class Header extends ExcelComponent {
     return `
     <input type="text" class="input" value="${title}">
       <div>
-          <div class="button">
-              <i class="material-icons">delete</i>
+          <div class="button" data-button="remove">
+              <i class="material-icons" data-button="remove">delete</i>
           </div>
-          <div class="button">
-              <i class="material-icons">exit_to_app</i>
+          <div class="button" data-button="exit">
+              <i class="material-icons" data-button="exit">exit_to_app</i>
           </div>
       </div>
     `
