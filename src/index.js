d84f0a98071d0bcb769abf9b26f3150d73c0171b
RTBGist <YourDeveloper@mail.ru>
2020-12-15 23:45:13 +0700

    Routing implemented (#7)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import './scss/index.scss'
-import {Excel} from '@/components/excel/Excel';
-import {Header} from '@/components/header/Header';
-import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Formula} from '@/components/formula/Formula';
-import {List} from '@/components/list/List';
-import {createStore} from '@core/createStore';
-import {rootReducer} from '@/redux/rootReducer';
-import {storage, debounce} from '@core/utils';
-import {initialState} from '@/redux/initialState';
+import {Router} from '@core/routes/Router';
+import {DashboardPage} from '@/pages/DashboardPage';
+import {ExcelPage} from '@/pages/ExcelPage';
+// import {Excel} from '@/components/excel/Excel';
+// import {Header} from '@/components/header/Header';
+// import {Toolbar} from '@/components/toolbar/Toolbar';
+// import {Formula} from '@/components/formula/Formula';
+// import {List} from '@/components/list/List';
+// import {createStore} from '@core/createStore';
+// import {rootReducer} from '@/redux/rootReducer';
+// import {storage, debounce} from '@core/utils';
+// import {initialState} from '@/redux/initialState';
 
-const store = createStore(rootReducer, initialState)
-
-const stateListener = debounce((state) => {
-  console.log('APP state:', state)
-  storage('excel-state', state)
-}, 300)
-
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, List],
-  store,
-});
-
-excel.render();
+new Router('#app', {
+  dashboard: DashboardPage,
+  excel: ExcelPage,
+})
