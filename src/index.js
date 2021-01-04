4a659634a166e8aaff6b9023bf631507fbe325ec
RTBGist <YourDeveloper@mail.ru>
2021-01-05 02:58:37 +0700

    Create unit testing env (#8)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import './scss/index.scss'
 import {Router} from '@core/routes/Router';
 import {DashboardPage} from '@/pages/DashboardPage';
 import {ExcelPage} from '@/pages/ExcelPage';
-// import {Excel} from '@/components/excel/Excel';
-// import {Header} from '@/components/header/Header';
-// import {Toolbar} from '@/components/toolbar/Toolbar';
-// import {Formula} from '@/components/formula/Formula';
-// import {List} from '@/components/list/List';
-// import {createStore} from '@core/createStore';
-// import {rootReducer} from '@/redux/rootReducer';
-// import {storage, debounce} from '@core/utils';
-// import {initialState} from '@/redux/initialState';
 
 new Router('#app', {
   dashboard: DashboardPage,
